9b2d892e1970d77626ca31dbd36a1f842603d8b7
sujin <rkdthd1234@naver.com>
2022-05-11 03:14:13 +0900

    인풋 러프하게 구현

--- Lotto/Lotto/Lotto/View/InputView.swift
+++ Lotto/Lotto/Lotto/View/InputView.swift
@@ -11,6 +11,8 @@ struct InputView {
     
     private enum QuestionText: UserInformable {
         case purchaseMoney
+        case manualBuyCount
+        case manualNumbers
         case winningNumbers
         case bonusNumber
         
@@ -18,6 +20,10 @@ struct InputView {
             switch self {
             case .purchaseMoney:
                 return "구입금액을 입력해 주세요."
+            case .manualBuyCount:
+                return "수동으로 구매할 로또 수를 입력해 주세요."
+            case .manualNumbers:
+                return "수동으로 구매할 번호를 입력해 주세요."
             case .winningNumbers:
                 return "지난 주 당첨 번호를 입력해 주세요."
             case .bonusNumber:
@@ -39,14 +45,38 @@ struct InputView {
         return purchaseMoney
     }
     
+    func recieveManualBuyCount(inBudget purchaseMoney: Int) throws -> Int {
+        userGuider.printGuide(for: QuestionText.manualBuyCount)
+        
+        let manualBuyCount = try recieveInt()
+        //todo 0 이랑 purchaseMoney/1000 사이여야함
+        return manualBuyCount
+    }
+    
+    func recieveManualLottos(for manualBuyCount: Int) throws -> [Lotto] {
+        userGuider.printGuide(for: QuestionText.manualNumbers)
+        
+        //todo manualButyCount 는 1 이상
+        let lottos: [Lotto] = try (1...manualBuyCount).map { _ -> Lotto in
+            let lotto: Lotto = try recieveLotto()
+            return lotto
+        }
+        return lottos
+    }
+    
     func recieveWinningLotto() throws -> Lotto {
         userGuider.printGuide(for: QuestionText.winningNumbers)
         
+        let lotto: Lotto = try recieveLotto()
+        return lotto
+    }
+    
+    private func recieveLotto() throws -> Lotto {
         let userInput: String? = readLine()
         let unwrappedUserInput: String = try stringConverter.unwrapOptional(from: userInput)
-        let winningNumbers = try userInputConverter.convertToWinningNumbers(from: unwrappedUserInput)
-        let winningLotto = try Lotto(numbers: winningNumbers)
-        return winningLotto
+        let lottoNumbers: [Int] = try userInputConverter.convertToLottoNumbers(from: unwrappedUserInput)
+        let lotto = try Lotto(numbers: lottoNumbers)
+        return lotto
     }
     
     func recieveBonusNumber(in winningLotto: Lotto) throws -> Int {
--- Lotto/Lotto/Lotto/main.swift
+++ Lotto/Lotto/Lotto/main.swift
@@ -7,20 +7,25 @@
 import Foundation
 
 do {
+    //todo 로또 얼마 예산으로 살거고,, 몇개를 수동으로 뽑을거고.. 자동으로 어케 할거고 이런거 좀 모아놓는데 있으면 좋을듯
     let inputView = InputView()
     let purchaseMoney: Int = try inputView.recievePurchaseMoney()
-
+    let manualBuyCount: Int = try inputView.recieveManualBuyCount(inBudget: purchaseMoney)
+    let manualLottos: [Lotto] = try inputView.recieveManualLottos(for: manualBuyCount)
+    
+    let purchasableCount = purchaseMoney / Lotto.Constants.price
+    let automaticBuyCount: Int = purchasableCount - manualBuyCount
     let resultView = ResultView()
-    let purchaseCount = purchaseMoney / Lotto.Constants.price
-    resultView.printPurchaseCount(with: purchaseCount)
-
-    let lottos: [Lotto] = try buyLottos(for: purchaseCount)
-    resultView.printLottos(for: lottos)
+    resultView.printPurchaseCount(manual: manualBuyCount, automatic: automaticBuyCount)
+    let automaticLottos: [Lotto] = try buyAutomaticLottos(for: automaticBuyCount)
+    
+    let allLottos: [Lotto] = manualLottos + automaticLottos
+    resultView.printLottos(for: allLottos)
     
     let winningLotto: Lotto = try inputView.recieveWinningLotto()
     let bonusNumber: Int = try inputView.recieveBonusNumber(in: winningLotto)
     
-    let lottoResult: LottoResult = try lottoResult(lottos: lottos,
+    let lottoResult: LottoResult = try lottoResult(lottos: allLottos,
                                                    winningLotto: winningLotto,
                                                    bonusNumber: bonusNumber)
     resultView.printWinningStatistics(with: lottoResult)
@@ -29,7 +34,7 @@ do {
     errorView.printError(for: error)
 }
 
-private func buyLottos(for purchaseCount: Int) throws -> [Lotto] {
+private func buyAutomaticLottos(for purchaseCount: Int) throws -> [Lotto] {
     let lottoGenerator = LottoGenerator()
     let lottos: [Lotto] = try (1...purchaseCount)
         .map { _ in
